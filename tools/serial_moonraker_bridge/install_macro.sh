20f633286c70d25cdfd39e97b111dc6682b3ba99
Leonel Dos Santos Jeronimo <27760806+shishu94@users.noreply.github.com>
2024-03-15 16:45:51 +0100

    Update install_macro.sh

--- tools/serial_moonraker_bridge/install_macro.sh
+++ tools/serial_moonraker_bridge/install_macro.sh
@@ -3,7 +3,7 @@
 sed -i "s|<USER_DIR>|$PWD|g" serial_moonraker_bridge.cfg
 cp serial_moonraker_bridge.cfg ~/printer_data/config
 
-sed -i "1s|^|[include serial_moonraker_bridge.cfg]\n|g" /~/printer_data/config/printer.cfg
+sed -i "1s|^|[include serial_moonraker_bridge.cfg]\n|g" ~/printer_data/config/printer.cfg
 
 
 
