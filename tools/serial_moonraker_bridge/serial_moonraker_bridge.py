cbf8499827f2b12b2ce377e80d91559adb0e7467
Leonel Dos Santos Jeronimo <27760806+shishu94@users.noreply.github.com>
2024-03-15 20:26:43 +0100

    Update serial_moonraker_bridge.py

--- tools/serial_moonraker_bridge/serial_moonraker_bridge.py
+++ tools/serial_moonraker_bridge/serial_moonraker_bridge.py
@@ -23,10 +23,21 @@ logging.basicConfig(level=numeric_level)
 
 moonraker_address=args.moonraker
 
+def checkMoonraker():
+    conn = http.client.HTTPConnection(moonraker_address)                                                       
+    conn.request("POST", "/printer/info")                    
+    response = conn.getresponse()                                                         
+    conn.close()
+    if response.reason != 'OK':
+        exit()
+      
+checkMoonraker()
+
 palette_ser = serial.Serial()
 palette_ser.port = args.serial
 palette_ser.baudrate = args.baudrate
 palette_ser.open()
+
 
 def listenPalette():
 	print("Listening palette")
@@ -35,7 +46,7 @@ def listenPalette():
 		logging.info('<P3>'+ line + '</P3>')                              
 		conn = http.client.HTTPConnection(moonraker_address)              
 		if line == "___START_JOB___":                                                          
-			conn.request("POST", "/printer/print/start?filename=RemotePrint.gcode")                    
+			conn.request("POST", "/printer/print/start?filename=Remote.gcode")                    
 		else:
 			conn.request("POST", "/printer/gcode/script?script="+quote(line))
 			
@@ -47,15 +58,15 @@ def listenPalette():
 	
 
 async def listenMoonrakerAsync():
-	print("Listening moonraker")
-	async with websockets.connect('ws://'+moonraker_address+'/websocket') as moonraker_ws:
-		while True:
-			response = await moonraker_ws.recv()
-			response_object = json.loads(response)
-			if response_object['method'] == "notify_gcode_response":
-				for item in response_object['params']:
-					logging.info('<MoonRaker>' + item + '</Moonraker>')
-					palette_ser.write((item+'\n').encode('utf-8'))
+    print("Listening moonraker")
+    async with websockets.connect('ws://'+moonraker_address+'/websocket') as moonraker_ws:
+        while True:
+            response = await moonraker_ws.recv()
+            response_object = json.loads(response)
+            if response_object['method'] == "notify_gcode_response":
+                for item in response_object['params']:
+                    logging.info('<MoonRaker>' + item + '</Moonraker>')
+                    palette_ser.write((item+'\n').encode('utf-8'))
 
 	
 Process(target=listenPalette).start()
