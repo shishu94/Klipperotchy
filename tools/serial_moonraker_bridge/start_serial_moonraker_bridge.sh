968247946b444e0586473eb46a7e6f6466f83b8c
Leonel Dos Santos Jeronimo <27760806+shishu94@users.noreply.github.com>
2024-03-15 16:59:33 +0100

    Update start_serial_moonraker_bridge.sh

--- tools/serial_moonraker_bridge/start_serial_moonraker_bridge.sh
+++ tools/serial_moonraker_bridge/start_serial_moonraker_bridge.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "baudrate=$1 serial=$2 moonraker=$3"
-nohup /usr/bin/python3 serial_moonraker_bridge.py --baudrate=$1 --serial=$2 --moonraker=$3 &
+nohup /usr/bin/python3 <USER_DIR>/serial_moonraker_bridge.py --baudrate=$1 --serial=$2 --moonraker=$3 &
